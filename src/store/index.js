e7a0740c322456498d0de1cd178f9ad0aeda99b5
toho36 <toho0036@gmail.com>
2023-05-17 15:24:21 +0200

    245 Adding auth slice

--- src/store/index.js
+++ src/store/index.js
@@ -21,9 +21,26 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActuins = authSlice.actions;
 export default store;
