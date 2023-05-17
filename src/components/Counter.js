77bb41972d0aa4e45e51a7cf238d9dd648451c9d
toho36 <toho0036@gmail.com>
2023-05-17 10:05:36 +0200

    239 Multiplate state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -15,12 +16,14 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
